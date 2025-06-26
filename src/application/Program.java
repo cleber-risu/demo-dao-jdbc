1db6059398ebe195f11a54ec1bf436c56d0ea3c0
cleber <cleber.risu@outlook.com>
2025-06-26 11:29:47 -0300

    Seller entity class

---
+++ src/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+import java.time.LocalDate;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department obj = new Department(1, "Books");
+		
+		Seller seller = new Seller(21, "Bob", "bob@email.com", LocalDate.now(), 3000.0, obj);
+		
+		System.out.println(seller);
+	}
+
+}
